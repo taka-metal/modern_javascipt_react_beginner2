ca5b5d4a37093d0f722a2d8397db93e10ce286c2
taka-metal <takatai.studyhub@gmail.com>
2024-08-07 20:08:06 +0900

    コンポーネントの使い方を知る

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,12 @@
 // 一般的にはテンプレートを使用する
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client" 
+// コンポーネント名は大文字始まりの名前でないとエラーになる
+import { App } from "./App"
 
 // index.htmlのbodyのdivタグを参照する
 const rootElement = document.getElementById("root")
 const root = createRoot(rootElement)
-
-// 今は画面の要素は関数で定義する。これをタグのように記述することで利用可能
-const App = () => {
-  // returnで返すものは一つの要素にしないといけない
-  return (
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  )
-}
 
 root.render(
   <StrictMode>
