f65ec6f8d6de2e27b0232647dc4c0224f2a31c9d
taka-metal <takatai.studyhub@gmail.com>
2024-08-08 21:28:40 +0900

    Reactでのイベントやスタイルの扱い方を知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 // 今は画面の要素は関数で定義する。これをタグのように記述することで利用可能
 export const App = () => {
-    // returnで返すものは一つの要素にしないといけない
-    return (
-      <>
-        <h1>こんにちは！</h1>
-        <p>お元気ですか？</p>
-      </>
-    )
+  // HTMLタグの中が長ったらしくなりそうだったらreturnの前に関数を定義しておく
+  const onClickButton = () => alert()
+  // スタイルを当てることもできる
+  // ただしこのようなインラインスタイルは煩雑になるためあまり使われない
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
   }
+  // returnで返すものは一つの要素にしないといけない
+  return (
+    <>
+      <h1 style={{color: "red"}}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  )
+}
